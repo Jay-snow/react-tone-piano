bbef4f9a94b2dcbeef0e68b6fe0e0e6a1928787d
Marcus Snow <marcusjsnow1@gmail.com>
2022-10-04 10:15:36 -0400

    added tonejs, can play c3 through b3

--- src/layout/piano_wrapper.js
+++ src/layout/piano_wrapper.js
@@ -1,18 +1,26 @@
+import * as Tone from 'tone'
+
 
 function PianoWrapper() {
 
+    function clickHandler(e) {
+        let note = e.target.dataset.note
+        const synth = new Tone.Synth().toDestination();
 
+        synth.triggerAttackRelease(note, "8n");
+
+    }
 
 
     return (
         <div className="piano-wrapper">
-            <div className="white-key"> </div>
-            <div className="white-key"> </div>
-            <div className="white-key"> </div>
-            <div className="white-key"> </div>
-            <div className="white-key"> </div>
-            <div className="white-key"> </div>
-            <div className="white-key"> </div>
+            <div onMouseDown={clickHandler} data-note={"C3"} className="white-key"> </div>
+            <div onMouseDown={clickHandler} data-note={"D3"} className="white-key"> </div>
+            <div onMouseDown={clickHandler} data-note={"E3"} className="white-key"> </div>
+            <div onMouseDown={clickHandler} data-note={"F3"} className="white-key"> </div>
+            <div onMouseDown={clickHandler} data-note={"G3"} className="white-key"> </div>
+            <div onMouseDown={clickHandler} data-note={"A3"} className="white-key"> </div>
+            <div onMouseDown={clickHandler} data-note={"B3"} className="white-key"> </div>
             <div className="black-key" style={{ marginLeft: 6 }}> </div>
             <div className="black-key" style={{ marginLeft: 36 }}> </div>
             <div className="black-key" style={{ marginLeft: 96 }}> </div>
