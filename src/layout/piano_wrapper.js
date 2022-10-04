fe6b8484b04f00c935b0d7228722cb7d2ffa6a03
Marcus Snow <marcusjsnow1@gmail.com>
2022-10-04 09:57:03 -0400

    Removed createkeys function. Now hardcoded.

--- src/layout/piano_wrapper.js
+++ src/layout/piano_wrapper.js
@@ -2,29 +2,22 @@
 function PianoWrapper() {
 
 
-    const createKeys = () => {
-
-        let final_html = []
-
-        for (let i = 0; i < 12; i++) {
-            final_html.push(
-                <div className="black-key" key={i} style={{ marginLeft: 30 * i }}> </div>
-            )
-            final_html.push(
-                <div className="white-key" key={i}> </div>
-            )
-        }
-
-        return final_html
-
-    }
-
-
 
 
     return (
         <div className="piano-wrapper">
-            {createKeys()}
+            <div className="white-key"> </div>
+            <div className="white-key"> </div>
+            <div className="white-key"> </div>
+            <div className="white-key"> </div>
+            <div className="white-key"> </div>
+            <div className="white-key"> </div>
+            <div className="white-key"> </div>
+            <div className="black-key" style={{ marginLeft: 6 }}> </div>
+            <div className="black-key" style={{ marginLeft: 36 }}> </div>
+            <div className="black-key" style={{ marginLeft: 96 }}> </div>
+            <div className="black-key" style={{ marginLeft: 126 }}> </div>
+            <div className="black-key" style={{ marginLeft: 156 }}> </div>
         </div>
     );
 }
