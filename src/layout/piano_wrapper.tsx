718d0287cebcd1e560902ef8e744a555aaf4fd65
Marcus Snow <marcusjsnow1@gmail.com>
2022-10-05 08:04:23 -0400

    Can now perform piano rolling

--- src/layout/piano_wrapper.tsx
+++ src/layout/piano_wrapper.tsx
@@ -1,28 +1,50 @@
 import * as Tone from 'tone'
+import React, { useState } from 'react';
+
+const synth = new Tone.Synth().toDestination();
+document.addEventListener("mouseup", e => {
+    synth.triggerRelease();
+})
 
 
 
 function PianoWrapper() {
 
+    const [notes, setNotes] = useState(["C3", "D3", "E3", "F3", "G3", "A3", "B3"])
 
+    let isPlaying = false;
 
     function clickHandler(e: any) {
-        let note = e.target.dataset.note
-        const synth = new Tone.Synth().toDestination();
-        synth.triggerAttackRelease(note, "8n");
+        let note = e.target.dataset.note;
+        let isPlaying = (e.target.dataset.playing === 'true');
+        console.log(isPlaying)
+        if (e.buttons === 1 && !isPlaying) {
+            Tone.start();
+            synth.triggerAttack(note);
+            e.target.dataset.playing = 'true';
+            console.log(e.target.dataset.playing)
+        }
 
+
+
+    }
+
+    function releaseAttack(e: any) {
+        e.target.dataset.playing = 'false';
+        synth.triggerRelease();
+    }
+
+    function test() {
+        console.log("HEllo");
     }
 
 
     return (
         <div className="piano-wrapper">
-            <div onMouseOver={clickHandler} data-note={"C3"} className="white-key"> </div>
-            <div onMouseEnter={clickHandler} data-note={"D3"} className="white-key"> </div>
-            <div onMouseEnter={clickHandler} data-note={"E3"} className="white-key"> </div>
-            <div onMouseDown={clickHandler} data-note={"F3"} className="white-key"> </div>
-            <div onMouseDown={clickHandler} data-note={"G3"} className="white-key"> </div>
-            <div onMouseDown={clickHandler} data-note={"A3"} className="white-key"> </div>
-            <div onMouseDown={clickHandler} data-note={"B3"} className="white-key"> </div>
+            {notes.map((note, i) =>
+                <button draggable="false" key={i} onMouseLeave={releaseAttack} onMouseMove={clickHandler} data-playing={false} data-note={note} className="white-key"> </button>
+            )}
+
             <div className="black-key" style={{ marginLeft: 6 }}> </div>
             <div className="black-key" style={{ marginLeft: 36 }}> </div>
             <div className="black-key" style={{ marginLeft: 96 }}> </div>
